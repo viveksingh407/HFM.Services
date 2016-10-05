f6a575d3514d93a84fcb537301ba2a5a0cfd27a7
viveksingh407 <viveksingh407@hotmail.com>
2016-10-05 01:32:04 -0700

    Added BOFA checkings account statement parser.

--- HFM.Business/Utilities/bofaCA.cs
+++ HFM.Business/Utilities/bofaCA.cs
@@ -5,143 +5,203 @@ using System.Text;
 using System.Threading.Tasks;
 using System.IO;
 using HFM.Business.Data;
+using System.Text.RegularExpressions;
+using HFM.Data.Repository.Implementation;
 
 namespace HFM.Business.Utilities
 {
     public static class bofaCA
     {
-        public class Taxes
+        public static IList<Statement> BofAStatements(bool addToDB)
         {
-            public string TotalTaxAmount { get; set; }
-            public string PostingDate { get; set; }
-            public string Description { get; set; }
-            public int TransactionNumber { get; set; }
-            public int AccountNumber { get; set; }
-            public decimal TransactionAmount { get; set; }
-        }
+            var statementText = new List<string>();
+            var transactions = new List<Statement>() as IList<Statement>;
+            var allTransactions = new List<Statement>() as IList<Statement>;
+            var statements = new List<string>() as IList<string>;
+            var allStatements = new List<string>() as IList<string>;
+            var path = @"M:\Data\Bank Statements\BofA\Checking\Text";
 
-        public class Payments
-        {
-            public string PaymentDate { get; set; }
-            public string PostingDate { get; set; }
-            public string Description { get; set; }
-            public int TransactionNumber { get; set; }
-            public int AccountNumber { get; set; }
-            public decimal TransactionAmount { get; set; }
-        }
+            var startIndex = -1;
+            var endIndex = -1;
 
-        public class Statement
-        {
-            public string PurchaseDate { get; set; }
-            public string PostingDate { get; set; }
-            public string Description { get; set; }
-            public int TransactionNumber { get; set; }
-            public int AccountNumber { get; set; }
-            public decimal TransactionAmount { get; set; }
-        }
+            var statementPaths = Directory.GetFiles(path, "*.txt");
 
-        public static IList<Statement> FileQuery()
-        {
-            var statementPath = "M:\\Data\\HFM\\HFM Bank Statements\\eStmt_2016-07-15.txt";
-            var completeStatementText = File.ReadAllLines(statementPath)
-                    .Where(n => !string.IsNullOrEmpty(n))
-                    .Select(n => n.TrimStart())
-                    .ToList();
+            var year = string.Empty;
 
-            var payments = GetPaymentsInfo(completeStatementText);
-            var totalTax = GetTaxInfo(completeStatementText);
-            var paymentAmount = GetTotalPaymentAmount(completeStatementText);
-            var transactions = GetTransactions(completeStatementText);
+            foreach (var statementPath in statementPaths)
+            {
+                year = Regex.Split(Path.GetFileName(statementPath), @"[^0-9\.]+").Where(n => n.StartsWith("201")).FirstOrDefault();
+                statementText = File.ReadAllLines(statementPath).Where(n => !string.IsNullOrEmpty(n)).Select(n => n.Trim()).ToList();
 
-            var totalCalculatedAmount = transactions.Select(n => n.TransactionAmount).Sum();
+                startIndex = statementText.FindIndex(n => n.StartsWith("Your checking account"));
+                endIndex = statementText.Count() - 1;
 
+                GetDeposits(statementText.GetRange(startIndex, endIndex - startIndex));
+
+            }
             return transactions;
         }
 
-        private static IList<Payments> GetPaymentsInfo(List<string> paymentDetails)
+        private static IList<string> GetDeposits(List<string> depositTransactionBlock)
         {
-            var data = new List<Payments>() as IList<Payments>;
+            var depositList = new List<string>() as IList<string>;
 
-            var paymentHeaderIdentifier = paymentDetails.IndexOf("Payments and Other Credits");
-            var purchaseHeaderIdentifier = paymentDetails.IndexOf("Purchases and Adjustments");
+            var startIndex = depositTransactionBlock.IndexOf("Deposits and other additions") + 2;
+            var endIndex = depositTransactionBlock.IndexOf("Withdrawals and other subtractions", startIndex);
 
-            var payments = paymentDetails.Skip(paymentHeaderIdentifier + 1).Take((purchaseHeaderIdentifier - 2) - paymentHeaderIdentifier).ToList();
+            var deposits = depositTransactionBlock.GetRange(startIndex, endIndex - startIndex);
 
-            foreach (var lineItem in payments)
+            deposits.Remove("Withdrawals and other subtractions");
+            deposits.Remove(deposits.Find(n => n.StartsWith("Withdrawals and other subtractions")));
+            deposits.Remove(deposits.Find(n => n.StartsWith("$")));
+            deposits.Remove(deposits.Find(n => n.StartsWith("Total deposits and other additions")));
+            deposits.Remove(deposits.Find(n => n.StartsWith("Amount")));
+
+            var description = new List<string>() as IList<string>;
+            var extraDescription = new List<string>() as IList<string>;
+            var cost = new List<string>() as IList<string>;
+            var tempDeposit = string.Empty;
+
+            foreach (var deposit in deposits)
             {
-                var transactions = lineItem
-                    .Split(new string[] { "  " }, StringSplitOptions.RemoveEmptyEntries)
-                    .ToList();
+                double temp = 0.0;
 
-                if (transactions.Count() > 5)
+                if (deposit.Contains("/"))
                 {
-                    data.Add(new Payments()
+                    var tempAll = deposit.Trim().Split(new char[] { ' ' });
+                    tempDeposit = deposit;
+
+                    if (double.TryParse(tempAll[tempAll.Length - 1], out temp))
                     {
-                        PaymentDate = Convert.ToString(transactions[0]),
-                        PostingDate = Convert.ToString(transactions[1]),
-                        Description = string.Join(" ", transactions.Skip(2).Take((transactions.Count() - 3) - 2)),
-                        TransactionNumber = Convert.ToInt32(transactions[transactions.Count() - 3]),
-                        AccountNumber = Convert.ToInt32(transactions[transactions.Count() - 2]),
-                        TransactionAmount = Convert.ToDecimal(transactions[transactions.Count() - 1].TrimStart().Substring(1))
-                    });
+                        cost.Add(tempAll[tempAll.Length - 1]);
+                        tempDeposit = deposit.Replace(tempAll[tempAll.Length - 1], string.Empty);
+                    }
+
+                    description.Add(tempDeposit);
+                }
+                else if (double.TryParse(deposit, out temp))
+                {
+                    cost.Add(Convert.ToString(temp));
+                }
+                else
+                {
+                    extraDescription.Add(deposit);
                 }
             }
 
-            return data;
-        }
+            int count = 0;
+            var tempFirstDescription = string.Empty;
+            var tempDescription = string.Empty;
+            var tempCost = string.Empty;
 
-        private static decimal GetTaxInfo(List<string> paymentDetails)
-        {
-            var interestHeaderIdentifier = paymentDetails.FindIndex(n => n.StartsWith("TOTAL INTEREST FOR THIS PERIOD"));
-            var totalTax = Convert.ToDecimal(paymentDetails[interestHeaderIdentifier].Remove(0, "TOTAL INTEREST FOR THIS PERIOD".Length).TrimStart().Substring(1));
-
-            return totalTax;
-        }
-
-        private static decimal GetTotalPaymentAmount(List<string> paymentDetails)
-        {
-            var interestHeaderIdentifier = paymentDetails.FindIndex(n => n.StartsWith("TOTAL INTEREST FOR THIS PERIOD"));
-            var purchaseHeaderIdentifier = paymentDetails.IndexOf("Purchases and Adjustments");
-
-            var totalTax = Convert.ToDecimal(paymentDetails[interestHeaderIdentifier].Remove(0, "TOTAL INTEREST FOR THIS PERIOD".Length).TrimStart().Substring(1));
-
-            var totalPaymentAmountText = paymentDetails.Skip(purchaseHeaderIdentifier - 1).Take(1).First();
-            var totalPaymentAmount = Convert.ToDecimal(totalPaymentAmountText.Substring(totalPaymentAmountText.IndexOf("$") + 1));
-
-            return totalTax;
-        }
-
-        private static IList<Statement> GetTransactions(List<string> paymentDetails)
-        {
-            var data = new List<Statement>() as IList<Statement>;
-            var purchaseHeaderIdentifier = paymentDetails.IndexOf("Purchases and Adjustments");
-
-            var statement = paymentDetails
-                .Skip(purchaseHeaderIdentifier + 1)
-                .Where(n => (n.Contains("0723") || n.Contains("2680")) && n.Substring(0, 5).Contains("/"))
-                .ToList();
-
-            foreach (var lineItem in statement)
+            foreach (var all in description)
             {
-                var transactions = lineItem
-                    .Split(new string[] { "  " }, StringSplitOptions.RemoveEmptyEntries)
-                    .ToList();
+                tempFirstDescription = string.Empty;
+                tempDescription = string.Empty;
+                tempCost = string.Empty;
 
-                if (transactions.Count() > 5)
+                if (extraDescription.Any() && extraDescription.Count() >= count - 1)
                 {
-                    data.Add(new Statement()
+                    tempDescription = string.Concat(extraDescription[count], " ");
+                    extraDescription.RemoveAt(count);
+                }
+
+                if (cost.Any() && cost.Count() >= count - 1)
+                {
+                    tempCost = cost[count];
+                    cost.RemoveAt(count);
+                }
+
+                tempFirstDescription = string.Concat(all, " ");
+
+                depositList.Add(string.Concat(tempFirstDescription, tempDescription, tempCost).Trim());
+            }
+
+            return depositList;
+        }
+
+        private static Statement GetStatement(string statement, bool isMultiMonthStatement, string year)
+        {
+            string[] data = new string[4];
+            var statementData = new Statement();
+
+            year = (isMultiMonthStatement && statement.Substring(0, 2) == "12") ? Convert.ToString(Convert.ToInt32(year) - 1) : year;
+
+            var purchaseDate = GetDate(statement.Substring(0, 5), year);
+            var postingDate = GetDate(statement.Substring(6, 5), year);
+
+            var remainingStatement = statement.Substring(12);
+
+            //get amount as the regex will extract all th decimals from the string.
+            var temp = Regex.Split(remainingStatement, @"[^0-9\.]+");
+
+            if (!(temp.Distinct().Count() == 1 && string.IsNullOrEmpty(temp.Distinct().First())))
+            {
+                var transactionNumber = Convert.ToInt32(temp[temp.Length - 2]);
+                var amount = Convert.ToDecimal(temp[temp.Length - 1]);
+
+                amount = remainingStatement.Contains(amount + "CR") ? amount * -1 : amount;
+
+                var description = remainingStatement.Substring(0, remainingStatement.IndexOf(temp[temp.Length - 2])).Trim();
+
+                statementData = new Statement()
+                {
+                    PurchaseDate = purchaseDate,
+                    Description = description,
+                    TransactionAmount = amount,
+                    BalanceAmount = 0,
+                    PostingDate = postingDate,
+                    TransactionDate = purchaseDate,
+                    AccountType = 3,
+                    TransactionNumber = transactionNumber
+                };
+            }
+
+            return statementData;
+        }
+
+        private static bool AddStatements(IList<Statement> statements)
+        {
+            bool isSuccess = false;
+
+            BankStatementsRepository repo = new BankStatementsRepository();
+
+            try
+            {
+                var a = statements.Where(n => n.AccountType == 0);
+                foreach (var data in statements)
+                {
+                    repo.Add(new BankStatementsRepository()
                     {
-                        PurchaseDate = Convert.ToString(transactions[0]),
-                        PostingDate = Convert.ToString(transactions[1]),
-                        Description = string.Join(" ", transactions.Skip(2).Take((transactions.Count() - 3) - 2)),
-                        TransactionNumber = Convert.ToInt32(transactions[transactions.Count() - 3]),
-                        AccountNumber = Convert.ToInt32(transactions[transactions.Count() - 2]),
-                        TransactionAmount = Convert.ToDecimal(transactions[transactions.Count() - 1])
+                        BankAccountTypeId = data.AccountType,
+                        TransactionDate = data.PurchaseDate,
+                        PaymentDate = data.PurchaseDate,
+                        PostingDate = data.PostingDate,
+                        TransactionDescription = data.Description,
+                        TransactionNumber = data.TransactionNumber,
+                        Amount = data.TransactionAmount,
+                        BalanceAmount = data.BalanceAmount
                     });
                 }
             }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
 
-            return data;
+            return isSuccess;
+        }
+
+        private static string GetYear(string dateRange)
+        {
+            string transactionDate = string.Empty;
+
+            return Regex.Split(dateRange, @"[^0-9\.]+").Where(n => n.StartsWith("201")).FirstOrDefault();
+        }
+
+        private static DateTime GetDate(string datePart, string yearPart)
+        {
+            return Convert.ToDateTime(string.Concat(datePart, "/", yearPart));
         }
     }
 }
