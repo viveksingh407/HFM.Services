4ecdfd2101b82452794036957f95d597b87091a6
viveksingh407 <viveksingh407@hotmail.com>
2016-10-07 00:32:36 -0700

    Added bank statement view.

---
+++ HFM.Web/Controllers/HomeScreenController.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using HFM.Web.Models;
+using Newtonsoft.Json;
+
+namespace HFM.Web.Controllers
+{
+    public class HomeScreenController : Controller
+    {
+        // GET: Dashboard
+        public ActionResult Index()
+        {
+            BankStatementModel model = new BankStatementModel();
+            model.GetBankStatementDetails();
+            return View();
+        }
+
+        [HttpGet]
+        public JsonResult GetStatements()
+        {
+            BankStatementModel model = new BankStatementModel();
+            var data = model.GetBankStatementDetails();
+            var serializedTransactionData = JsonConvert.SerializeObject(data);
+
+            return new JsonResult() { Data = serializedTransactionData, JsonRequestBehavior = JsonRequestBehavior.AllowGet };
+        }
+
+        [HttpGet]
+        public JsonResult GetStatementsByDate(DateTime fromDate)
+        {
+            BankStatementModel model = new BankStatementModel();
+            var data = model.GetBankStatementDetails(fromDate);
+            var serializedTransactionData = JsonConvert.SerializeObject(data);
+
+            return new JsonResult() { Data = serializedTransactionData, JsonRequestBehavior = JsonRequestBehavior.AllowGet };
+        }
+
+        [HttpGet]
+        public JsonResult GetStatementsBetweenDates(DateTime fromDate, DateTime toDate)
+        {
+            BankStatementModel model = new BankStatementModel();
+            var data = model.GetBankStatementDetails(fromDate, toDate);
+            var serializedTransactionData = JsonConvert.SerializeObject(data);
+
+            return new JsonResult() { Data = serializedTransactionData, JsonRequestBehavior = JsonRequestBehavior.AllowGet };
+        }
+
+        [HttpPost]
+        public ActionResult AddStatement(BankStatementModel data)
+        {
+            BankStatementModel model = new BankStatementModel();
+            model.AddStatement(data);
+
+            var transactions = model.GetBankStatementDetails(DateTime.Today);
+
+            return PartialView("Statements", transactions);
+        }
+    }
+}
---
+++ HFM.Web/Scripts/HFMScripts/HomeScreen.js
@@ -0,0 +1,32 @@
+﻿$('#formHomeScreen').ready(function () {
+    var url = 'bankstatements/getstatements';
+    var str = '';
+
+    $.ajax({
+        type: "GET",
+        url: url,
+        success: function (data) {
+            $.each($.parseJSON(data), function (i, item) {
+                str += "<tr><td>" + item["Amount"] + "</td>" +
+                "<td>" + item["BalanceAmount"] + "</td>" +
+                "<td>" + item["BankAccountTypeId"] + "</td>" +
+                "<td>" + item["BankAccountTypeName"] + "</td>" +
+                "<td>" + item["BankStatementId"] + "</td>" +
+                "<td>" + item["PaymentDate"] + "</td>" +
+                "<td>" + item["PostingDate"] + "</td>" +
+                "<td>" + item["TransactionDate"] + "</td>" +
+                "<td>" + item["TransactionDescription"] + "</td>" +
+                "<td>" + item["TransactionNumber"] + "</td><tr>"
+            });
+
+            var tableHeader = "<tr><th class=hide> BankStatementId </th> <th class=hide> BankAccountTypeId </th> <th> Bank Name </th> <th> Transaction Date </th> <th> Payment Date </th> <th> Posting Date </th> <th> Transaction Description </th> <th> Transaction Number </th> <th> Amount </th> <th> Balance Amount </th> <th> StoreName </th> </tr>";
+
+            var table = tableHeader + str;
+
+            $('#tblStatements').html(table);
+        },
+        failure: function (jqXHR, textStatus, error) {
+            console.log("Post error: " + error);
+        }
+    });
+})
